b9288588e0d26be154af9282015cf870eaa3d003
lhaerim <lhaerim1229@naver.com>
2024-07-01 04:46:23 +0900

    Feat: 유저 정보 가져오기 (추가) #12
    get 요청을 USER_INFO_URI으로 보냄, 그리고 응답을 가져옴. 응답의 본문을 flux로 반환
    
    추가로 공부해야 할 부분
    1. 왜 URL이 아닌 URI인지
    2. 요청 헤더 부분에 "Authorization"를 왜 추가했는지

--- src/main/java/WELLET/welletServer/member/KakaoUserInfo.java
+++ src/main/java/WELLET/welletServer/member/KakaoUserInfo.java
@@ -1,2 +1,22 @@
-package WELLET.welletServer.member;public class KakaoUserInfo {
+package WELLET.welletServer.member;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class KakaoUserInfo {
+
+    private final webClient webClient;
+    private static final String USER_INFO_URI= "https://localhost:8080/oauth2/kakao/callback";
+
+    public KakaoUserInfoResponse getUserInfo(String token) {
+        Flux<KakaoUserInfoResponse> response = webClient.get()
+                .URI(USER_INFO_URI)
+                .header("Authorization", "Bearer" + token)
+                .retrieve()
+                .bodyToFlux(KakaoUserInfoResponse.class);
+        return response.blockFirst();
+    }
 }
