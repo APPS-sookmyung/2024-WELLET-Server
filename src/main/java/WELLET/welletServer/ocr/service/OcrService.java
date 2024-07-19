c5869c23e437e5ab059bde3fd3c656853071753a
ajeong <ajung7038@naver.com>
2024-07-19 15:03:06 +0900

    Feat: OCR 이메일 추출 #19

--- src/main/java/WELLET/welletServer/ocr/service/OcrService.java
+++ src/main/java/WELLET/welletServer/ocr/service/OcrService.java
@@ -1,22 +1,32 @@
 package WELLET.welletServer.ocr.service;
 
+import WELLET.welletServer.card.exception.CardErrorCode;
+import WELLET.welletServer.card.exception.CardException;
 import WELLET.welletServer.ocr.dto.OcrRequest;
 import lombok.extern.slf4j.Slf4j;
 import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
+import net.sourceforge.tess4j.Word;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 @Slf4j
 @Transactional
 public class OcrService implements IOcrService {
 
-    private static final String filePath =  "C:/Users/ajung/Documents/GitHub/2024-WELLET-Server/src/main/resources/static/testFile.png";
+//    private static final String filePath =  "C:/Users/ajung/Documents/GitHub/2024-WELLET-Server/src/main/resources/static/testFile.png";
+    private static final String filePath =  "C:/Users/ajung/Documents/GitHub/2024-WELLET-Server/src/main/resources/static/testCardFile.jpg";
     @Override
     @Transactional
     public void getImageToText(OcrRequest dto) throws TesseractException, IOException {
@@ -27,9 +37,25 @@ public class OcrService implements IOcrService {
 
         ITesseract instance = new Tesseract();
         instance.setDatapath(IOcrService.model);
-        instance.setLanguage("kor");
-        String ImageToText = instance.doOCR(file);
+        instance.setLanguage("eng+kor");
+        String imageToText = instance.doOCR(file);
+        System.out.println(imageToText);
+        extractRegularExpression(imageToText);
+    }
 
-        System.out.println(ImageToText);
+    @Transactional
+    public void extractRegularExpression(String text) {
+        String patternString = "\\S+@\\w+\\.\\w+(\\.\\w+)?";
+
+        Pattern pattern = Pattern.compile(patternString);
+        Matcher matcher = pattern.matcher(text);
+
+        String email = "";
+
+        if (matcher.find()) {
+            email = String.valueOf(matcher.group());
+        }
+
+        System.out.println(email);
     }
 }
