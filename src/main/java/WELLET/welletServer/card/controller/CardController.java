6bab5f8fa2bba237f71075fad30eda8d6fd5c16e
ajeong <ajung7038@naver.com>
2024-08-02 22:25:21 +0900

    Feat: add CardController Swagger Annotation #32

--- src/main/java/WELLET/welletServer/card/controller/CardController.java
+++ src/main/java/WELLET/welletServer/card/controller/CardController.java
@@ -11,6 +11,10 @@ import WELLET.welletServer.common.response.BasicResponse;
 import WELLET.welletServer.common.response.ResponseUtil;
 import WELLET.welletServer.member.domain.Member;
 import WELLET.welletServer.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -20,12 +24,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/cards")
 @RequiredArgsConstructor
+@Tag(name = "명함", description = "Card API")
 public class CardController {
     private final CardService cardService;
     private final MemberService memberService;
     private final CategoryCardService categoryCardService;
 
     @PostMapping("/{member_id}")
+    @Operation(summary = "명함 생성")
+    @Parameters({
+            @Parameter(name = "member_id", example = "1"),
+    })
     public BasicResponse<CardResponse> create(@PathVariable(name = "member_id") Long memberId, @Valid @RequestBody CardSaveDto dto) {
         Member member = memberService.findMember(memberId);
         if (dto.getCategoryNames() != null) {
@@ -36,22 +45,35 @@ public class CardController {
     }
 
     @GetMapping
+    @Operation(summary = "전체 명함 조회")
     public BasicResponse<CardCountResponseDto> findAllCards() {
         return ResponseUtil.success(cardService.findAllCard());
     }
 
     @GetMapping("/{card_id}")
+    @Operation(summary = "명함 단건 조회")
+    @Parameters({
+            @Parameter(name = "card_id", example = "1"),
+    })
     public BasicResponse<CardResponse> findCard(@PathVariable(name = "card_id") Long cardId) {
         return ResponseUtil.success(cardService.findCard(cardId));
     }
 
     @PutMapping("/{card_id}")
+    @Operation(summary = "명함 수정")
+    @Parameters({
+            @Parameter(name = "card_id", example = "1"),
+    })
     public BasicResponse<CardUpdateDto> updateCard(@PathVariable Long card_id, @Valid @RequestBody CardUpdateDto dto) {
         CardUpdateDto cardUpdateDto = cardService.updateCard(card_id, dto);
         return ResponseUtil.success(cardUpdateDto);
     }
 
     @DeleteMapping("/{card_id}")
+    @Operation(summary = "명함 삭제")
+    @Parameters({
+            @Parameter(name = "card_id", example = "1"),
+    })
     public BasicResponse<String> deleteCard(@PathVariable Long card_id) {
         Card card = cardService.findOne(card_id);
         categoryCardService.delete(card);
@@ -59,6 +81,10 @@ public class CardController {
     }
 
     @PostMapping
+    @Operation(summary = "명함 동시 삭제")
+    @Parameters({
+            @Parameter(name = "cards_id", example = "[1, 2]"),
+    })
     public BasicResponse<String> deleteCardList(@RequestBody List<Long> cards_id) {
         cardService.deleteCardList(cards_id);
         return ResponseUtil.success("명함 동시 삭제에 성공하였습니다. 명함 id : " + cards_id);
