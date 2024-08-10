fd9582ce4da566b6ba7952a93cab2202db6292f0
hayeyoung <hayeyoung4031@naver.com>
2024-08-11 01:28:26 +0900

    Feat: 검색기능 예외처리

--- src/main/java/WELLET/welletServer/card/controller/CardController.java
+++ src/main/java/WELLET/welletServer/card/controller/CardController.java
@@ -90,9 +90,18 @@ public class CardController {
         return ResponseUtil.success("명함 동시 삭제에 성공하였습니다. 명함 id : " + cards_id);
     }
 
+//    @GetMapping("/search")
+//    @Operation(summary = "이름으로 명함 검색")
+//    public BasicResponse<CardCountResponseDto> searchCardsByName(@RequestParam(value="keyword") String keyword) {
+//        return ResponseUtil.success(cardService.searchCardsByName(keyword));
+//    }
     @GetMapping("/search")
     @Operation(summary = "이름으로 명함 검색")
-    public BasicResponse<CardCountResponseDto> searchCardsByName(@RequestParam(value="keyword") String keyword) {
-        return ResponseUtil.success(cardService.searchCardsByName(keyword));
-    }
+    public BasicResponse<CardCountResponseDto> searchCardsByName(
+        @RequestParam(value = "keyword", required = false) String keyword) {
+        if (keyword == null || keyword.isEmpty()) {
+            return ResponseUtil.success(cardService.findAllCard());
+        } else {
+            return ResponseUtil.success(cardService.searchCardsByName(keyword));
+    }}
 }
