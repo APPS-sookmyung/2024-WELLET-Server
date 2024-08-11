046aba778c3d25c96c968d055f0ab14b9f516ceb
ajeong <ajung7038@naver.com>
2024-08-11 13:12:33 +0900

    Feat: 그룹명 중복 예외 처리 #39

--- src/main/java/WELLET/welletServer/category/exception/CategoryErrorCode.java
+++ src/main/java/WELLET/welletServer/category/exception/CategoryErrorCode.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum CategoryErrorCode {
-    CATEGORY_NOT_FOUND("그룹을 찾을 수 없습니다.");
+    CATEGORY_NOT_FOUND("그룹을 찾을 수 없습니다."),
+    CATEGORY_DUPLICATE("이미 존재하는 그룹입니다.");
 
     private final String message;
 }
