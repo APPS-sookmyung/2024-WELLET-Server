a21c282552c632836bcb5b5d57da7542f96d699c
ajeong <ajung7038@naver.com>
2024-08-16 17:01:15 +0900

    Fix: 명함 생성 api 수정 #57

--- src/main/java/WELLET/welletServer/card/domain/Card.java
+++ src/main/java/WELLET/welletServer/card/domain/Card.java
@@ -26,11 +26,10 @@ public class Card extends BaseTimeEntity {
 
     @NotBlank
     private String name;
-
     private String position;
+    private String email;
 
     @NotBlank
-    private String email;
     private String phone;
     private String tel;
     private String department;
