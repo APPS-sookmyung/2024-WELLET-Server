8cb79beb5a34c2edd6516b1d83a627cfe5ab2b7b
lhaerim <lhaerim1229@naver.com>
2024-09-21 17:53:00 +0900

    Feat: 브라우저 접속 컨트롤러 #!2

---
+++ src/main/java/WELLET/welletServer/kakaologin/controller/KakaoLoginPageController.java
@@ -0,0 +1,27 @@
+package WELLET.welletServer.kakaologin.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth/kakao/callback")
+public class KakaoLoginPageController {
+
+    @Value("${kakao.client_id}")
+    private String client_id;
+
+    @Value("${kakao.redirect_uri}")
+    private String redirect_uri;
+
+    @Value("/page")
+    public String loginPage(Model model) {
+        String location = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=\"+client_id+\"&redirect_uri="+redirect_uri;
+        model.addAttribute("location", location);
+
+        return "login";
+    }
+}
+
+//https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=856122255feeea21d537c0225f6c658a&redirect_uri=https://localhost:8080/auth/kakao/callback 접속 가능
