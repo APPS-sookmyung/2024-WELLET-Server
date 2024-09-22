00ca4cd66c8fed576a53c8dbbde3f9c2c23ee282
lhaerim <lhaerim1229@naver.com>
2024-09-22 19:32:32 +0900

    Feat: 로그인 페이지 생성 #12

--- src/main/java/WELLET/welletServer/kakaologin/controller/KakaoLoginPageController.java
+++ src/main/java/WELLET/welletServer/kakaologin/controller/KakaoLoginPageController.java
@@ -3,6 +3,7 @@ package WELLET.welletServer.kakaologin.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,7 +16,7 @@ public class KakaoLoginPageController {
     @Value("${kakao.redirect_uri}")
     private String redirect_uri;
 
-    @Value("/page")
+    @GetMapping("/page")
     public String loginPage(Model model) {
         String location = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=\"+client_id+\"&redirect_uri="+redirect_uri;
         model.addAttribute("location", location);
@@ -25,3 +26,5 @@ public class KakaoLoginPageController {
 }
 
 //https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=856122255feeea21d537c0225f6c658a&redirect_uri=https://localhost:8080/auth/kakao/callback 접속 가능
+
+// https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=856122255feeea21d537c0225f6c658a&redirect_uri=https://0dd7-59-15-81-194.ngrok-free.app/auth/kakao/callback 접속 가능
