c4cea2adb54ae3fb4a9296ec54b9198603417ef5
hayeyoung <hayeyoung4031@naver.com>
2024-09-29 21:01:01 +0900

    Fix: 충돌 해결

--- src/main/java/WELLET/welletServer/card/Repository/CardRepository.java
+++ src/main/java/WELLET/welletServer/card/Repository/CardRepository.java
@@ -5,6 +5,7 @@ import WELLET.welletServer.category.domain.Category;
 import WELLET.welletServer.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
