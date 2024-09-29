474c78b315ec390e60888991116279b0558e7e7e
hayeyoung <hayeyoung4031@naver.com>
2024-09-29 18:36:25 +0900

    Fix: 충돌 해결

--- src/main/java/WELLET/welletServer/card/domain/Card.java
+++ src/main/java/WELLET/welletServer/card/domain/Card.java
@@ -52,9 +52,6 @@ public class Card extends BaseTimeEntity {
     @JoinColumn(name = "category_id")
     private Category category;
 
-    @OneToOne(mappedBy = "card", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private CardImage cardImage;
-
     private Long ownerId;
 
     @Builder
