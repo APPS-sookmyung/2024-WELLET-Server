72d34b4c8ecb101718686f385b93f73a379a79d2
lhaerim <lhaerim1229@naver.com>
2024-12-22 19:25:32 +0900

    Feat: jwt 바디 담기 #127

--- src/main/java/WELLET/welletServer/kakaologin/controller/KakaoLoginController.java
+++ src/main/java/WELLET/welletServer/kakaologin/controller/KakaoLoginController.java
@@ -37,7 +37,7 @@ public class KakaoLoginController {
 
 
     @GetMapping("/callback")
-    public ResponseEntity<?> callback(@RequestParam("code") String code, HttpServletResponse response) throws IOException {
+    public String callback(@RequestParam("code") String code, HttpServletResponse response) throws IOException {
         try {
             // 1. 카카오에서 accessToken 받아오기
             String accessToken = kakaoService.getAccessTokenFromKakao(code);
@@ -60,20 +60,15 @@ public class KakaoLoginController {
             // 4. JWT 생성
             String jwtToken = jwtService.generateToken(member);  // 생성된 또는 업데이트된 사용자로 JWT 생성
 
-            // 5. 쿠키에 JWT 저장
-            Cookie jwtCookie = new Cookie("jwtToken", jwtToken);
-            jwtCookie.setHttpOnly(true);  // JavaScript로 쿠키에 접근 불가
-//            jwtCookie.setSecure(true);    // HTTPS에서만 전송
-            jwtCookie.setMaxAge(60 * 60 * 24);  // 쿠키 유효 시간 설정
-            jwtCookie.setPath("/");  // 쿠키를 모든 경로에 적용
-            response.addCookie(jwtCookie);
+            return jwtToken;
 
             // 6. 성공적으로 로그인 완료 시 OK 응답
-            return ResponseEntity.ok("로그인 성공");
+            // return ResponseEntity.ok("로그인 성공");
 
         } catch (Exception e) {
             log.error("로그인 처리 중 오류 발생", e);
-            return new ResponseEntity<>("로그인 처리 중 오류", HttpStatus.INTERNAL_SERVER_ERROR);
+            // return new ResponseEntity<>("로그인 처리 중 오류", HttpStatus.INTERNAL_SERVER_ERROR);
+            return "로그인 처리 중 오류 발생" + e;
         }
     }
 }
