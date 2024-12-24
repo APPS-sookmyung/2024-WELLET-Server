cc21b7e758721021463ff15f832d35d366d2eaaa
ajeong <ajung7038@naver.com>
2024-12-24 22:44:47 +0900

    Fix: cookie 수정 #149

--- src/main/java/WELLET/welletServer/kakaologin/controller/KakaoLoginController.java
+++ src/main/java/WELLET/welletServer/kakaologin/controller/KakaoLoginController.java
@@ -62,19 +62,32 @@ public class KakaoLoginController {
             // 4. JWT 생성
             String jwtToken = jwtService.generateToken(member);  // 생성된 또는 업데이트된 사용자로 JWT 생성
 //
-//            // 5. 쿠키에 JWT 저장
-            Cookie jwtCookie = new Cookie("jwtToken", jwtToken);
-            jwtCookie.setHttpOnly(true);  // JavaScript로 쿠키에 접근 불가
-            jwtCookie.setSecure(true);    // HTTPS에서만 전송
-            jwtCookie.setMaxAge(60 * 60 * 24);  // 쿠키 유효 시간 설정
-            jwtCookie.setPath("/");  // 쿠키를 모든 경로에 적용
+////            // 5. 쿠키에 JWT 저장
+//            Cookie jwtCookie = new Cookie("jwtToken", jwtToken);
+//            jwtCookie.setHttpOnly(true);  // JavaScript로 쿠키에 접근 불가
+//            jwtCookie.setSecure(false);    // HTTPS에서만 전송
+//            jwtCookie.setMaxAge(60 * 60 * 24);  // 쿠키 유효 시간 설정
+//            jwtCookie.setPath("/");  // 쿠키를 모든 경로에 적용
+//
+//            jwtCookie.setDomain("wellet.netlify.app");
+//            response.addCookie(jwtCookie);
 
-            jwtCookie.setDomain("wellet.netlify.app");
-            response.addCookie(jwtCookie);
+            ResponseCookie jwtCookie = ResponseCookie.from("jwtToken", jwtToken)
+                    .httpOnly(true)
+                    .secure(true)
+                    .path("/")
+                    .sameSite("None")
+                    .maxAge(60 * 60 * 24)
+                    .build();
+            response.addHeader(HttpHeaders.SET_COOKIE, jwtCookie.toString());
+
 
             // 리다이렉트 URL 설정
             String redirectUrl = "https://wellet.netlify.app";  // 원하는 프론트엔드 URL
+//            String redirectUrl = "http://localhost:8000";  // 원하는 프론트엔드 URL
             response.sendRedirect(redirectUrl);
+
+
             return "로그인 성공";
 
         } catch (Exception e) {
