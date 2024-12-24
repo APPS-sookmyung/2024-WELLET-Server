25fe0cd1f0cc3c75bee6f08a860d7fa539150fb0
ajeong <ajung7038@naver.com>
2024-12-25 06:34:37 +0900

    Fix: 리다이렉트 url 수정 #149

--- src/main/java/WELLET/welletServer/kakaologin/controller/KakaoLoginController.java
+++ src/main/java/WELLET/welletServer/kakaologin/controller/KakaoLoginController.java
@@ -96,7 +96,7 @@ public class KakaoLoginController {
 
 
             // 리다이렉트 URL 설정
-            String redirectUrl = frontendUrl;  // 원하는 프론트엔드 URL
+            String redirectUrl = frontendUrl + "/home";  // 원하는 프론트엔드 URL
             response.sendRedirect(redirectUrl);
 
 
