792fe65ebbb7dd15c12f569a39708b9b98dcad44
ajeong <ajung7038@naver.com>
2025-01-26 21:28:02 +0900

    Fix: 쿠키 설정 변경 #221

--- src/main/java/WELLET/welletServer/kakaologin/controller/KakaoLoginController.java
+++ src/main/java/WELLET/welletServer/kakaologin/controller/KakaoLoginController.java
@@ -100,8 +100,8 @@ public class KakaoLoginController {
             jwtCookie.setSecure(true);  // HTTPS에서만 전송 X
             jwtCookie.setMaxAge(60 * 60 * 24);  // 쿠키 유효 시간 설정
             jwtCookie.setPath("/");
-//            jwtCookie.setDomain(domain);
-            response.addCookie(jwtCookie);
+            jwtCookie.setDomain(domain);
+//            response.addCookie(jwtCookie);
 
 //            response.addHeader("Set-Cookie", "jwtToken=" + jwtToken + "; Path=/; HttpOnly; Secure; Max-Age=" + (60 * 60 * 24) + "; SameSite=None");
 
