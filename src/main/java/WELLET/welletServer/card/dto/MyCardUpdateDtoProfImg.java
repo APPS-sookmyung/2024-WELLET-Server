c14e85a299998c4190b961db97eef0da1a4f1726
이해림 <haerim@ihaelim-ui-MacBookPro.local>
2025-02-16 16:58:57 +0900

    Update MyCardUpdateDtoProfImg.java

--- src/main/java/WELLET/welletServer/card/dto/MyCardUpdateDtoProfImg.java
+++ src/main/java/WELLET/welletServer/card/dto/MyCardUpdateDtoProfImg.java
@@ -7,5 +7,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Getter @Builder
 public class MyCardUpdateDtoProfImg {
-    @Schema(description = "프로필 이미지") private MultipartFile profImg;
+    @Schema(description = "프로필 이미지")
+    private MultipartFile profImg;
 }
