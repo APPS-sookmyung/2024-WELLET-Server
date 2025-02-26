a884e7bb77a179958f03f881d911c7fcbf418432
ajeong <ajung7038@naver.com>
2025-02-26 14:10:31 +0900

    Fix: 토큰 에러 코드 수정 #263

--- src/main/java/WELLET/welletServer/kakaologin/exception/TokenErrorCode.java
+++ src/main/java/WELLET/welletServer/kakaologin/exception/TokenErrorCode.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum TokenErrorCode {
 
-    INVALID_JWT("유효하지 않은 토큰입니다.");
+    INVALID_JWT("유효하지 않은 토큰입니다."),
+    NOT_FOUND_JWT("토큰을 찾을 수 없습니다.");
 
     private final String message;
 
